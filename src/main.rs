d0269416537f408918153ceb4e2112b8cef8d1fa
Anup Dhamala <anupdhml+git@gmail.com>
2019-11-14 15:36:42 -0500

    Add tower-lsp example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use futures::future;
+use jsonrpc_core::{BoxFuture, Result};
+use serde_json::Value;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{LanguageServer, LspService, Printer, Server};
+
+#[derive(Debug, Default)]
+struct Backend;
+
+impl LanguageServer for Backend {
+    type ShutdownFuture = BoxFuture<()>;
+    type SymbolFuture = BoxFuture<Option<Vec<SymbolInformation>>>;
+    type ExecuteFuture = BoxFuture<Option<Value>>;
+    type CompletionFuture = BoxFuture<Option<CompletionResponse>>;
+    type HoverFuture = BoxFuture<Option<Hover>>;
+    type HighlightFuture = BoxFuture<Option<Vec<DocumentHighlight>>>;
+
+    fn initialize(&self, _: &Printer, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult::default())
+    }
+
+    fn initialized(&self, printer: &Printer, _: InitializedParams) {
+        printer.log_message(MessageType::Info, "server initialized!");
+    }
+
+    fn shutdown(&self) -> Self::ShutdownFuture {
+        Box::new(future::ok(()))
+    }
+
+    fn symbol(&self, _: WorkspaceSymbolParams) -> Self::SymbolFuture {
+        Box::new(future::ok(None))
+    }
+
+    fn execute_command(&self, _: &Printer, _: ExecuteCommandParams) -> Self::ExecuteFuture {
+        Box::new(future::ok(None))
+    }
+
+    fn completion(&self, _: CompletionParams) -> Self::CompletionFuture {
+        Box::new(future::ok(None))
+    }
+
+    fn hover(&self, _: TextDocumentPositionParams) -> Self::HoverFuture {
+        Box::new(future::ok(None))
+    }
+
+    fn document_highlight(&self, _: TextDocumentPositionParams) -> Self::HighlightFuture {
+        Box::new(future::ok(None))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, messages) = LspService::new(Backend::default());
+    let handle = service.close_handle();
+    let server = Server::new(stdin, stdout)
+        .interleave(messages)
+        .serve(service);
+
+    tokio::run(handle.run_until_exit(server));
 }

742796dd5eaae120f373e0e74ad71d46f45e11da
Matthias Wahl <matthiaswahl@m7w3.de>
2022-09-19 11:11:23 +0200

    Add basic initialization test
    Signed-off-by: Matthias Wahl <matthiaswahl@m7w3.de>

--- .github/scripts/bump.sh
+++ .github/scripts/bump.sh
@@ -3,4 +3,8 @@
 old=$1
 new=$2
 
+# update dependencies in Cargo.toml
 sed -e "s/^tremor-script = { version = \"${old}\"/tremor-script = { version = \"${new}\"/" -i "Cargo.toml"
+
+# update server version is backend tests
+sed -e "s/^    const VERSION: \&str = \"${old}\";/    const VERSION: \&str = \"${new}\";/" -i "src/backend.rs"
